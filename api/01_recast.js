a72d7b7652515d284336c0d1842a0f2fdb13ba47
paradox903 <jadhavkaran903@gmail.com>
2022-02-17 13:12:01 +0530

    demo

--- api/01_recast.js
+++ api/01_recast.js
@@ -7,7 +7,7 @@ describe("verify the login functionality", function () {
     cy.get("#login-form > div:nth-child(5) > div > div > div > button").click();
     cy.url().should('contain', "login");
   });
-  it("test login with invalid credentials", function () {
+  it.only("test login with invalid credentials", function () {
     cy.visit("https://app.recast.studio/auth/login/");
     cy.get("#login-form_email").type("jadhavkaran903@gmail.com");
     cy.get("#login-form_password").type("Jadhav124");
